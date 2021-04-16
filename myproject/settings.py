2ae265c427ddab981e5d5f7f2bf8f23930e34836
ronaldomalla <sagarmalla03@gmail.com>
2021-04-16 16:53:11 +0545

    white install

--- myproject/settings.py
+++ myproject/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 from django.contrib.messages import constants as messages
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -134,3 +135,7 @@ STATICFILES_DIRS=[
 MESSAGE_TAGS = {
     messages.ERROR:'warning'
 }
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+django_heroku.settings(locals())

5786244b77b840e0cc8aa4e6aca1e5fdbcd8d96b
ronaldomalla <sagarmalla03@gmail.com>
2021-04-16 17:03:09 +0545

    top

--- myproject/settings.py
+++ myproject/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-import django_heroku
+
 
 from pathlib import Path
 from django.contrib.messages import constants as messages
@@ -138,4 +138,3 @@ MESSAGE_TAGS = {
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-django_heroku.settings(locals())
